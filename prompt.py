424cd7627495878da6e2aa22b24370984347469b
vanihtbrt <yovanivitanovahtbrt@gmail.com>
2024-02-27 14:52:11 +0700

    Commit Tugas Art of Prompt Engineering

---
+++ prompt.py
@@ -0,0 +1,6 @@
+nama = "Yovani Vitanova"
+jurusan = "Teknik Elektro"
+kampus = "Universitas Maritim Raja Ali Haji"
+print("Hai teman-teman!, kenalin aku " + nama + " dari jurusan " + jurusan + 
+      " di " + kampus)
+print("Salam kenal semuanya!")
